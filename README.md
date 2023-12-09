57e92d5c07f0413f88d6c2006d5caabd2656be8a
fisher <yuxinyong@163.com>
2023-12-09 10:36:45 +0800

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## Zelin Accounting
+
+Zelin Accounting
+
+#### License
+
+MIT
---
+++ zelin_ac/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Zelin Accounting",
+			"type": "module",
+			"label": _("Zelin Accounting")
+		}
+	]
